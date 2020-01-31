a107934064c706cfd56673f68d5844edab9033be
Yusuke-Xicun <joh.tourism@gmail.com>
2020-01-31 13:24:35 +0900

    Initial Commit

---
+++ AddCity3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AddCity3
+//
+//  Created by 西村友佑 on 2020/01/31.
+//  Copyright © 2020 西村友佑. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ AddCity3Tests/AddCity3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  AddCity3Tests.swift
+//  AddCity3Tests
+//
+//  Created by 西村友佑 on 2020/01/31.
+//  Copyright © 2020 西村友佑. All rights reserved.
+//
+
+import XCTest
+@testable import AddCity3
+
+class AddCity3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
